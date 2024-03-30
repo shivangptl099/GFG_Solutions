e7f9e8245b3038d86619de2ec85af71a4ccce617
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-03-30 12:40:41 +0530

    Create 32_Minimum_element_in_BST.cpp

---
+++ 32_Minimum_element_in_BST.cpp
@@ -0,0 +1,9 @@
+class Solution {
+  public:
+    int minValue(Node* root) {
+        if(!root) return -1;
+        while(root->left)
+            root = root -> left;
+        return root -> data;
+    }
+};

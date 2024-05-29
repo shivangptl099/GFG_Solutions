dde99b4d57974d1ea5293345035774d0661e3afb
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-05-29 23:27:27 +0530

    Create 29_05_24_Geek_and_its_Game_of_Coins.cpp

---
+++ 29_05_24_Geek_and_its_Game_of_Coins.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    int findWinner(int n, int x, int y) {
+        int dp[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            if(i - 1 >= 0 && dp[i - 1] == 0) dp[i] = 1;
+            else if(i - x >= 0 && dp[i - x] == 0) dp[i] = 1;
+            else if(i - y >= 0 && dp[i - y] == 0) dp[i] = 1;
+            else dp[i] = 0;
+        }
+        return dp[n];
+    }
+};

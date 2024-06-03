1137a67a170d8b53f6c17d0877d60000736505c9
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-06-03 20:07:00 +0530

    Create 03_06_24_Trail_of_ones.cpp

---
+++ 03_06_24_Trail_of_ones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+  public:
+    int numberOfConsecutiveOnes(int n) {
+        if(n == 2) return 1;
+        int a = 0, b = 1, ans = 1, mod = 1e9+7;
+        for(int i = 3; i <= n; i++){
+            int c = (a + b) % mod;
+            a = b;
+            b = c;
+            ans = (((ans * 2) % mod) + c) % mod;
+        }
+        return ans;
+    }
+};

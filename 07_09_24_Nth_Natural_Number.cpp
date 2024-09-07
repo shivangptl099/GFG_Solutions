cb5d1a5b9f4e002909242ad81083a36848f63dd0
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-09-07 23:27:27 +0530

    Create 07_09_24_Nth_Natural_Number.cpp

---
+++ 07_09_24_Nth_Natural_Number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    long long findNth(long long n) {
+        long long ans = 0, p = 1;
+        while(n > 0){
+            ans += p * (n % 9);
+            n /= 9;
+            p *= 10;
+        }
+        return ans;
+    }
+};

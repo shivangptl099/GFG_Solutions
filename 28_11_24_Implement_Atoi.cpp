9de10b7dcfdc0c773a68796d047c3cabedd01f90
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-11-28 13:18:36 +0530

    Create 28_11_24_Implement_Atoi.cpp

---
+++ 28_11_24_Implement_Atoi.cpp
@@ -0,0 +1,19 @@
+class Solution {
+  public:
+    int myAtoi(char *s) {
+        long ans = 0;
+        int i = 0, sign = 1;
+        while(s[i] == ' ') i++;
+        if(s[i] == '+' || s[i] == '-'){
+            sign = (s[i] == '-') ? -1 : 1;
+            i++;
+        }
+        while(s[i] >= '0' && s[i] <= '9'){
+            ans = (ans * 10) + (s[i] - '0');
+            if((sign * ans) >= INT_MAX) return INT_MAX;
+            if((sign * ans) <= INT_MIN) return INT_MIN;
+            i++;
+        }
+        return (int) sign * ans;
+    }
+};

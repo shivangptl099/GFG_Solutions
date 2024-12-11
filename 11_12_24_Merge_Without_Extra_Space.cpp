c60ee327bcffaf9f7819c3d999907c37493fd626
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2024-12-11 22:28:08 +0530

    Create 11_12_24_Merge_Without_Extra_Space.cpp

---
+++ 11_12_24_Merge_Without_Extra_Space.cpp
@@ -0,0 +1,17 @@
+class Solution {
+  public:
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        int i = a.size() - 1;
+        int j = 0;
+        while(i >= 0 && j < b.size()){
+            if(a[i] < b[j]) break;
+            else{
+                swap(a[i], b[j]);
+                i--;
+                j++;
+            }
+        }
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+    }
+};

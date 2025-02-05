a933b523ed773e8e4747cb2f912e03841fc8b070
Shivang B. Patel <117822313+shivangptl099@users.noreply.github.com>
2025-02-05 21:06:59 +0530

    Update and rename 13_09_24_Mirror_Tree.cpp_remain to 05_02_25_Mirror_Tree.cpp

--- 05_02_25_Mirror_Tree.cpp
+++ 05_02_25_Mirror_Tree.cpp
@@ -2,13 +2,9 @@ class Solution {
   public:
     // Function to convert a binary tree into its mirror tree.
     void mirror(Node* node) {
-        if (!node) return;
-        
+        if(!node) return;
+        swap(node->left, node->right);
         mirror(node->left);
         mirror(node->right);
-        
-        Node* temp = node->left;
-        node->left = node->right;
-        node->right = temp;
     }
 };
